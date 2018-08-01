b0bdfa3d672316526628f921b34e2f461b54acc5
Morgan Sipe <morgan.talk@gmail.com>
2018-08-01 18:11:01 -0400

    Added structure for problem 23

---
+++ PDot.UnitTests/Problems/Problem23/Problem23Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using PDot.Problems.Problems.Problem23;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDot.UnitTests.Problems.Problem23
+{
+  [TestFixture]
+  public class Problem23Tests {
+    [Test]
+    public void TestUsage() {
+      var runner = new Problem23Runner();
+    }
+  }
+}
