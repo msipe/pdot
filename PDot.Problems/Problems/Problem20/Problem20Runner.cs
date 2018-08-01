f64d26622885885a496be35388542052e4a1dd9a
Morgan Sipe <morgan.talk@gmail.com>
2018-08-01 17:39:32 -0400

    Fixed the tests

--- PDot.Problems/Problems/Problem20/Problem20Runner.cs
+++ PDot.Problems/Problems/Problem20/Problem20Runner.cs
@@ -8,9 +8,18 @@ namespace PDot.Problems.Problems.Problem20
 {
   public class Problem20Runner
   {
-    public int Execute(int incomingNum)
-    {
-      return 3628800;
+    public int Execute(int incomingNum) {
+      var workingPoint = incomingNum;
+      var reducedPoint = incomingNum;
+      var result = 1;
+      
+      for (var x = 0; x < incomingNum; x++) {
+        reducedPoint = workingPoint - 1;
+        result = result * workingPoint;
+        workingPoint = reducedPoint;
+      }
+
+      return result;
     }
   }
 }
--- PDot.UnitTests/Problems/Problem20/Problem20Tests.cs
+++ PDot.UnitTests/Problems/Problem20/Problem20Tests.cs
@@ -9,6 +9,8 @@ namespace PDot.UnitTests.Problems.Problem20
     public void TestUsage() {
       var runner = new Problem20Runner();
       Assert.That(runner.Execute(10), Is.EqualTo(3628800));
+      Assert.That(runner.Execute(5), Is.EqualTo(120));
+      Assert.That(runner.Execute(1), Is.EqualTo(1));
     }
   }
 }
