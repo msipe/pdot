0d3cec53adb25714adb3ca0780fa3ee14b760741
Morgan Sipe <morgan.talk@gmail.com>
2018-08-01 17:13:56 -0400

    Added basic test

---
+++ PDot.UnitTests/Problems/Problem20/Problem20Tests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace PDot.UnitTests.Problems.Problem20
+{
+  [TestFixture]
+  class Problem20Tests  {
+    [Test]
+    public void TestUsage() {
+      Assert.That(true, Is.False);
+    }
+  }
+}
