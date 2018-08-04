4bae9aa078605188d37d7f927cf6209c5ea62b5b
Morgan Sipe <morgan.talk@gmail.com>
2018-08-04 12:05:13 -0400

    Added the MysteryNumberFinder + Test

---
+++ PDot.UnitTests/Problems/Problem23/MysteryNumberFinderTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using PDot.Problems.Problems.Problem23;
+
+namespace PDot.UnitTests.Problems.Problem23
+{
+  [TestFixture]
+  class MySteryNumberFinderTest {
+    [Test]
+    public void TestFindFactorials() {
+      var finder = new MysteryNumberFinder();
+    }
+  }
+}
