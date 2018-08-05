9a356ac3563b12d5294931f8a978a07c0cffe340
Morgan Sipe <morgan.talk@gmail.com>
2018-08-05 19:43:48 -0400

    Added the driver + basic test

---
+++ PDot.Problems/Problems/Problem23/Driver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDot.Problems.Problems.Problem23
+{
+  public class Driver  {
+    public int[] FindSumsForAbundantNumbers(int max) {
+      return new[] { 1, 2, 3 };
+    }
+  }
+}
---
+++ PDot.UnitTests/Problems/Problem23/DriverTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using PDot.Problems.Problems.Problem23;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDot.UnitTests.Problems.Problem23
+{
+  [TestFixture]
+  class DriverTest {
+    [Test]
+    public void TestFindSumsForAbundantNumbers() {
+      var driver = new Driver();
+      Assert.That(driver.FindSumsForAbundantNumbers(10), Is.EqualTo(new[] {1,2,3}));
+    }
+  }
+}
