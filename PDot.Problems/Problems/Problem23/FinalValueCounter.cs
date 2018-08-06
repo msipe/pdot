f97f600fb52ac72d85597cbff00a6e814d70df1d
Morgan Sipe <morgan.talk@gmail.com>
2018-08-05 22:45:32 -0400

    Added functionality to the valueCounter

---
+++ PDot.Problems/Problems/Problem23/FinalValueCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDot.Problems.Problems.Problem23 {
+  public class FinalValueCounter {
+    public int Tally(int[] incoming) {
+      var result = 0;
+      for (var x =0; x < incoming.Length; x++) {
+        result += incoming[x];
+      }
+      return result;
+    }
+  }
+}
---
+++ PDot.UnitTests/Problems/Problem23/FinalValueCounterTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using PDot.Problems.Problems.Problem23;
+
+namespace PDot.UnitTests.Problems.Problem23
+{
+  [TestFixture]
+  public class FinalValueCounterTest {
+    [TestCase(new int[] {},0)]
+    [TestCase(new[] { 1},1)]
+    [TestCase(new[] { 1, 2 }, 3)]
+    [TestCase(new[] { 1,2,3}, 6)]
+    public void TestUsage(int[] incomingNums, int expected) {
+      var finalValueCounter = new FinalValueCounter();
+
+      Assert.That(finalValueCounter.Tally(incomingNums), Is.EqualTo(expected));
+    }
+  }
+}
