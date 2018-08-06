ae2f4c9f33b5b600044cf69d45b319be9d6f015f
Morgan Sipe <morgan.talk@gmail.com>
2018-08-05 22:36:04 -0400

    Fleshed out the possibleSumsFinderTest

--- PDot.UnitTests/Problems/Problem23/PossibleSumsFinderTest.cs
+++ PDot.UnitTests/Problems/Problem23/PossibleSumsFinderTest.cs
@@ -12,7 +12,14 @@ namespace PDot.UnitTests.Problems.Problem23
   class PossibleSumsFinderTest {
     [Test]
     public void TestFindPossibleSums() {
-     
+      var factorFinder = new FactorialFinder();
+      var abundantNumberFinder = new AbundantNumberFinder(factorFinder);
+      var collator = new SumCollator();
+      var possibleSumsFinder = new PossibleSumsFinder(abundantNumberFinder, collator);
+
+      Assert.That(possibleSumsFinder.FindSumsForAbundantNumbers(0), Is.EqualTo(new int[] {}));
+      Assert.That(possibleSumsFinder.FindSumsForAbundantNumbers(1), Is.EqualTo(new int[] {}));
+      Assert.That(possibleSumsFinder.FindSumsForAbundantNumbers(25), Is.EqualTo(new[] {24,30,32,36,36,38,40,42,44,48}));
     }
   }
 }
