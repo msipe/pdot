fcef03196e71c53ab39bcb82d39d23564e23e260
Morgan Sipe <morgan.talk@gmail.com>
2018-08-05 22:50:03 -0400

    Added the setup and dependencies:

--- PDot.Problems/Problems/Problem23/Problem23Runner.cs
+++ PDot.Problems/Problems/Problem23/Problem23Runner.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace PDot.Problems.Problems.Problem23 {
   public class Problem23Runner {
+    public Problem23Runner(PossibleSumsFinder possibleSumsFinder, FinalValueCounter valueCounter) {
+      mPossibleSumsFinder = possibleSumsFinder;
+      mValueCounter = valueCounter;
+    }
     public string Execute() {
       return "hey";
     }
+
+    private PossibleSumsFinder mPossibleSumsFinder;
+    private FinalValueCounter mValueCounter;
   }
 }
--- PDot.UnitTests/Problems/Problem23/Problem23RunnerTest.cs
+++ PDot.UnitTests/Problems/Problem23/Problem23RunnerTest.cs
@@ -6,7 +6,13 @@ namespace PDot.UnitTests.Problems.Problem23
   [TestFixture]
   public class Problem23RunnerTest {
     public void TestUsage() {
-      
+      var collator = new SumCollator();
+      var valueCounter = new FinalValueCounter();
+      var factorFinder = new FactorialFinder();
+      var abundantNumberFinder = new AbundantNumberFinder(factorFinder);
+      var possibleSumsFinder = new PossibleSumsFinder(abundantNumberFinder, collator);
+      var p23Runner = new Problem23Runner(possibleSumsFinder, valueCounter);
+
     }
   }
 }
