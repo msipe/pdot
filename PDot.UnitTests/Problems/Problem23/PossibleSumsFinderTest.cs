0e22ef4f504b39fe41e6f517ffe2901be661aed0
Morgan Sipe <morgan.talk@gmail.com>
2018-08-05 22:30:05 -0400

    Added the basic PossibleSumsFinderTest

---
+++ PDot.UnitTests/Problems/Problem23/PossibleSumsFinderTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using PDot.Problems.Problems.Problem23;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDot.UnitTests.Problems.Problem23
+{
+  [TestFixture]
+  class PossibleSumsFinderTest {
+    [Test]
+    public void TestFindPossibleSums() {
+     
+    }
+  }
+}
