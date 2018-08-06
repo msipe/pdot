297704b4ecc90fa555236d63e1c0800763ab45d5
Morgan Sipe <morgan.talk@gmail.com>
2018-08-05 22:28:53 -0400

    Cleansed program

--- PDot/Program.cs
+++ PDot/Program.cs
@@ -8,11 +8,7 @@ using System.Threading.Tasks;
 namespace PDot{
   class Program {
     static void Main(string[] args) {
-     // var problem = args[0];
-      //var runner = new Problem23Runner();
-      //runner.Execute();
-
-
+     
     }
     }
 }
