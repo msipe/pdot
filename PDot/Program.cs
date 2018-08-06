107fdc969cc3e9360cefb94546e033a40ac2d777
Morgan Sipe <morgan.talk@gmail.com>
2018-08-05 23:20:21 -0400

    Added some friendly usability to the pdot exe

--- PDot/Program.cs
+++ PDot/Program.cs
@@ -1,14 +1,41 @@
-﻿using PDot.Problems.Problems.Problem23;
+﻿using PDot.Problems.Problems.Problem20;
+using PDot.Problems.Problems.Problem23;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace PDot{
   class Program {
     static void Main(string[] args) {
-     
+      var problem = args[0];
+      var input = args[1];
+      SolveProblem(problem, input);
     }
+
+    private static void SolveProblem(string problem, string input) {
+      switch (problem) {
+        case "20":
+          new Problem20Runner().Execute(BigInteger.Parse(input));
+          break;
+        case "23":
+          ExecuteProblem23(int.Parse(input));
+          break;
+        default:
+          Console.WriteLine("unrecognized problem");
+          break;
+      }
     }
+
+    private static void ExecuteProblem23(int max) {
+      var collator = new SumCollator();
+      var factorFinder = new FactorFinder();
+      var abundantNumberFinder = new AbundantNumberFinder(factorFinder);
+      var valueCounter = new FinalValueCounter();
+      var possibleSumFinder = new PossibleSumsFinder(abundantNumberFinder, collator);
+      var mysteryNumberFinder = new MysteryNumberFinder(possibleSumFinder);
+      var problem23Runner = new Problem23Runner(mysteryNumberFinder, valueCounter);
+
+      problem23Runner.Execute(max);
+
+    }
+  }
 }
