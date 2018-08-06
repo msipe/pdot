566b9fab3870c89213756c7cb6a1468811c99f30
Morgan Sipe <morgan.talk@gmail.com>
2018-08-05 22:47:09 -0400

    The Problem23Runner is back and better than ever! (not really, it's just a skeleton

---
+++ PDot.Problems/Problems/Problem23/Problem23Runner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDot.Problems.Problems.Problem23 {
+  public class Problem23Runner {
+    public string Execute() {
+      return "hey";
+    }
+  }
+}
---
+++ PDot.UnitTests/Problems/Problem23/Problem23RunnerTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using PDot.Problems.Problems.Problem23;
+
+namespace PDot.UnitTests.Problems.Problem23
+{
+  [TestFixture]
+  public class Problem23RunnerTest {
+    public void TestUsage() {
+      
+    }
+  }
+}
