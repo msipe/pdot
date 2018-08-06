42a0ab73f16d311b50ff608220d772d1a386a767
Morgan Sipe <morgan.talk@gmail.com>
2018-08-06 09:00:53 -0400

    Cleanup

--- PDot.UnitTests/Problems/Problem23/FactorFinderTest.cs
+++ PDot.UnitTests/Problems/Problem23/FactorFinderTest.cs
@@ -8,12 +8,12 @@ namespace PDot.UnitTests.Problems.Problem23
     [Test]
     public void TestFindFactorials() {
       var finder = new FactorFinder();
-      Assert.That(finder.FindFactors(12), Is.EqualTo(new[] {1,2,3,4,6}));
-      Assert.That(finder.FindFactors(144), Is.EqualTo(new[] {1,2,3,4,6,8,9,12,16,18,24,36,48,72}));
-      Assert.That(finder.FindFactors(5), Is.EqualTo(new[] {1}));
-      Assert.That(finder.FindFactors(11), Is.EqualTo(new[] {1}));
-      Assert.That(finder.FindFactors(81), Is.EqualTo(new[] {1,3,9,27}));
       Assert.That(finder.FindFactors(0), Is.EqualTo(new int[] {}));
+      Assert.That(finder.FindFactors(5), Is.EqualTo(new[] { 1 }));
+      Assert.That(finder.FindFactors(11), Is.EqualTo(new[] { 1 }));
+      Assert.That(finder.FindFactors(12), Is.EqualTo(new[] {1,2,3,4,6}));
+      Assert.That(finder.FindFactors(81), Is.EqualTo(new[] { 1, 3, 9, 27 }));
+      Assert.That(finder.FindFactors(144), Is.EqualTo(new[] {1,2,3,4,6,8,9,12,16,18,24,36,48,72}));
     }
   }
 }
