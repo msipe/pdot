7fcf2aa94c72a0a7a35d08fb1c84872694ff807a
Morgan Sipe <morgan.talk@gmail.com>
2018-08-06 08:57:32 -0400

    Rename + cleanup

--- PDot.UnitTests/Problems/Problem20/Problem20RunnerTests.cs
+++ PDot.UnitTests/Problems/Problem20/Problem20RunnerTests.cs
@@ -5,14 +5,15 @@ using System.Numerics;
 namespace PDot.UnitTests.Problems.Problem20
 {
   [TestFixture]
-  class Problem20Tests  {
+  class Problem20RunnerTests  {
     [Test]
     public void TestUsage() {
       var runner = new Problem20Runner();
-      Assert.That(runner.Execute(100), Is.EqualTo(new BigInteger(648)));
-      Assert.That(runner.Execute(10), Is.EqualTo(new BigInteger(27)));
-      Assert.That(runner.Execute(5), Is.EqualTo(new BigInteger(3)));
       Assert.That(runner.Execute(1), Is.EqualTo(new BigInteger(1)));
+      Assert.That(runner.Execute(0), Is.EqualTo(new BigInteger(1)));
+      Assert.That(runner.Execute(5), Is.EqualTo(new BigInteger(3)));
+      Assert.That(runner.Execute(10), Is.EqualTo(new BigInteger(27)));
+      Assert.That(runner.Execute(100), Is.EqualTo(new BigInteger(648)));
     }
   }
 }
